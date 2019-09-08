3f5a1e9b9bb281aeaae28a894873a62add12799b
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-09-08 08:07:53 +0700

    Register custom routes

---
+++ QuanLySinhVien/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Routing;
+
+namespace QuanLySinhVien
+{
+	public class Global : System.Web.HttpApplication
+	{
+		protected void RegisterRoutes(RouteCollection routes)
+		{
+			routes.MapPageRoute("route-login", "Login", "~/Pages/Login.aspx");
+			routes.MapPageRoute("route-homepage", "", "~/Pages/Index.aspx");
+		}
+
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		protected void Session_Start(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_BeginRequest(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_AuthenticateRequest(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_Error(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Session_End(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void Application_End(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
